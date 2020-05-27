e9029d1301eac8b2c825ededb806e176aec1bcea
Ethan Setnik <esetnik@gmail.com>
2020-05-27 12:15:31 -0400

    Change entry and overlay

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const { addBabelPlugin, addWebpackPlugin, override } = require("customize-cra");
 const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const webpackDevClientEntry = require.resolve(
+  "react-dev-utils/webpackHotDevClient"
+);
+const paths = require("react-scripts/config/paths");
+
+const overrideEntry = () => (config) => {
+  config.entry = paths.appPath;
+};
 
 /**
  * @param {ReactRefreshPluginOptions} [options] Options for react-refresh-plugin.
  */
 const isDevelopment = process.env.NODE_ENV !== "production";
-const addReactRefresh = options =>
+const addReactRefresh = (options) =>
   override(
+    isDevelopment && overrideEntry(),
     isDevelopment && addBabelPlugin("react-refresh/babel"),
-    isDevelopment && addWebpackPlugin(new ReactRefreshPlugin(options))
+    isDevelopment &&
+      addWebpackPlugin(
+        new ReactRefreshPlugin({
+          overlay: {
+            entry: webpackDevClientEntry,
+          },
+          ...options,
+        })
+      )
   );
 
 module.exports = { addReactRefresh };
